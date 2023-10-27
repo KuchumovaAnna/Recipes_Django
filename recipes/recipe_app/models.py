0824a5d68e5cf758e80dab69bc69857e699693f9
KuchumovaAnna <ap199173@gmail.com>
2023-10-27 22:10:16 +0300

    6

--- recipes/recipe_app/models.py
+++ recipes/recipe_app/models.py
@@ -1,11 +1,14 @@
 from django.db import models
 
+from django.db import models
 
-class MyRecipe(models.Model):
-    title = models.CharField(max_length=200)
+
+class Recipe(models.Model):
+    title = models.CharField(max_length=100)
     ingredients = models.TextField()
     instructions = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.title
